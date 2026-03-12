6e634a526f011a9a6d60a3d9a896cb252ef0ed65
cheda-baseline <cedomir@getbaseline.com>
2026-03-12 14:28:28 +0100

    Initial Commit

---
+++ GitHubActionsTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubActionsTest
+//
+//  Created by Cedomir Stankov on 12. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubActionsTest/GitHubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsTestApp.swift
+//  GitHubActionsTest
+//
+//  Created by Cedomir Stankov on 12. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
