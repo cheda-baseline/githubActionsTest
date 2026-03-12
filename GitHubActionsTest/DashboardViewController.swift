433c43109a7dfab11c586a898ba33dc7714c48b8
cheda-baseline <cedomir@getbaseline.com>
2026-03-12 15:08:22 +0100

    Update DashboardViewController.swift
    func keyword tabbed once

--- GitHubActionsTest/DashboardViewController.swift
+++ GitHubActionsTest/DashboardViewController.swift
@@ -109,8 +109,8 @@ class DashboardViewController: UIViewController, UITableViewDelegate, UITableVie
         print("Selected: \(user.fullName)")
     }
 
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle==.delete { let user = users[indexPath.row]; repo.delete(byId: user.id); users.remove(at: indexPath.row); tableView.deleteRows(at: [indexPath], with: .fade) }
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete { let user = users[indexPath.row]; repo.delete(byId: user.id); users.remove(at: indexPath.row); tableView.deleteRows(at: [indexPath], with: .fade) }
     }
 }
 
